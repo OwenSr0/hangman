4efb283fcaa9dfd4b34e1ed0a840a43ebcd65c2b
OwenSr <owen.i.sanchez.r@gmail.com>
2022-05-19 17:06:19 -0500

    v2.1

--- js/hangman.js
+++ js/hangman.js
@@ -1,12 +1,6 @@
 const pe = document.querySelector("#ww");
 const pa = document.querySelector("#wl");
 var resets = [word1 = 0, error = 0,we = "", ingame = 0, nw = 0, wu = "", word = ""]
-var word1 = 0;
-var error = 0;
-var we = "";
-var ingame = 0;
-var nw = 0;
-var wu = "";
 
 window.addEventListener("keydown", function(event) {
     if(resets[3] == 1){
@@ -24,7 +18,7 @@ function exit(){
     document.getElementById("bars").style.display = "none";
     document.getElementById("ps").style.display = "none";
     document.getElementById("bottom").style.display = "none";
-    document.getElementById("starts").style.display = "flex";
+    document.getElementById("starts").style.display = "block";
      return resets[3] = 0;
 }
 
@@ -43,7 +37,6 @@ function ST(){
     pa.textContent = "";
     rl();
     Bard(resets[6].length);
-    //reseth();
     resets[3] = 1;
     resets[4] = resets[6].length;
     return resets[0,1,2,3];
@@ -55,14 +48,13 @@ function letter(k, w){
     }
     for(i = 0; i < w.length; i++){
     if(k == w[i]){
-        console.log("good")
         resets[6] = w.replaceAll(w[i],k)
         letters(k,resets[6]);
         return resets[6];
         }
 
     }
-    if(resets[1] == 7){
+    if(resets[1] == 6){
         alert("u lost")
         pa.textContent = "The word was " + resets[0];
         return resets[3] = 0;
@@ -71,14 +63,16 @@ function letter(k, w){
     pe.textContent = resets[2];
     resets[1]++
     hangman(resets[1]);
+    
+    return resets[1];
 }
   
 function letters(k,w){
     document.getElementById("ps").style.display = "flex";
     for(i = 0; i < w.length; i++){
         if(w[i] == k){
-            letterH[i].textContent = k;
-            letterH[i].style.display = "flex";
+            document.getElementById("l"+i).textContent = k;
+            document.getElementById("l"+i).style.display = "flex";
             resets[4] = resets[4] - 1;
         }
         if(resets[4] == 0){
@@ -91,122 +85,31 @@ function letters(k,w){
 }
 
 function rl(){
-    for(i = 0; i < letterH.length; i++){
-        letterH[i].style.display = "none";
+    for(i = 0; i < 8; i++){
+        document.getElementById("l"+i).style.display = "none";
     }
 }
 
-
-
-
-function hangman(er){
+function hangman(e){
     document.getElementById("hangman").style.display = "flex";
-    switch(er){
-        case 1:
-            document.getElementById("head").style.display = "none";
-            document.getElementById("body").style.display = "none";
-            document.getElementById("larm").style.display = "none";
-            document.getElementById("rarm").style.display = "none";
-            document.getElementById("lfe").style.display = "none";
-            document.getElementById("rfe").style.display = "none";
-        break;
-        case 2:
-            document.getElementById("head").style.display = "flex";
-            document.getElementById("body").style.display = "none";
-            document.getElementById("larm").style.display = "none";
-            document.getElementById("rarm").style.display = "none";
-            document.getElementById("lfe").style.display = "none";
-            document.getElementById("rfe").style.display = "none";
-        break;
-        case 3:
-            document.getElementById("head").style.display = "flex";
-            document.getElementById("body").style.display = "flex";
-            document.getElementById("larm").style.display = "none";
-            document.getElementById("rarm").style.display = "none";
-            document.getElementById("lfe").style.display = "none";
-            document.getElementById("rfe").style.display = "none";
-        break;
-        case 4:
-            document.getElementById("head").style.display = "flex";
-            document.getElementById("body").style.display = "flex";
-            document.getElementById("larm").style.display = "flex";
-            document.getElementById("rarm").style.display = "none";
-            document.getElementById("lfe").style.display = "none";
-            document.getElementById("rfe").style.display = "none";
-        break;
-        case 5:
-            document.getElementById("head").style.display = "flex";
-            document.getElementById("body").style.display = "flex";
-            document.getElementById("larm").style.display = "flex";
-            document.getElementById("rarm").style.display = "flex";
-            document.getElementById("lfe").style.display = "none";
-            document.getElementById("rfe").style.display = "none";
-        break;
-        case 6:
-            document.getElementById("head").style.display = "flex";
-            document.getElementById("body").style.display = "flex";
-            document.getElementById("larm").style.display = "flex";
-            document.getElementById("rarm").style.display = "flex";
-            document.getElementById("lfe").style.display = "flex";
-            document.getElementById("rfe").style.display = "none";
-        break;
-        case 7:
-            document.getElementById("head").style.display = "flex";
-            document.getElementById("body").style.display = "flex";
-            document.getElementById("larm").style.display = "flex";
-            document.getElementById("rarm").style.display = "flex";
-            document.getElementById("lfe").style.display = "flex";
-            document.getElementById("rfe").style.display = "flex";
-        break;
+    for(i = 0; i < 6; i++){
+        if(i < e){
+            document.getElementById("b"+ i).style.display = "flex"
+        }else{
+            document.getElementById("b"+ i).style.display = "none"
+         }
 
     }
 }
 
 function Bard(n){
     document.getElementById("bars").style.display = "flex";
-    switch(n){
-        case 3:
-            document.getElementById("bl4").style.display = "none";
-            document.getElementById("bl5").style.display = "none";
-            document.getElementById("bl6").style.display = "none";
-            document.getElementById("bl7").style.display = "none";
-            document.getElementById("bl8").style.display = "none";
-        break;
-        case 4:
-            document.getElementById("bl4").style.display = "flex";
-            document.getElementById("bl5").style.display = "none";
-            document.getElementById("bl6").style.display = "none";
-            document.getElementById("bl7").style.display = "none";
-            document.getElementById("bl8").style.display = "none";
-        break;
-        case 5:
-            document.getElementById("bl4").style.display = "flex";
-            document.getElementById("bl5").style.display = "flex";
-            document.getElementById("bl6").style.display = "none";
-            document.getElementById("bl7").style.display = "none";
-            document.getElementById("bl8").style.display = "none";
-        break;
-        case 6:
-            document.getElementById("bl4").style.display = "flex";
-            document.getElementById("bl5").style.display = "flex";
-            document.getElementById("bl6").style.display = "flex";
-            document.getElementById("bl7").style.display = "none";
-            document.getElementById("bl8").style.display = "none";
-        break;
-        case 7:
-            document.getElementById("bl4").style.display = "flex";
-            document.getElementById("bl5").style.display = "flex";
-            document.getElementById("bl6").style.display = "flex";
-            document.getElementById("bl7").style.display = "flex";
-            document.getElementById("bl8").style.display = "none";
-        break;
-        case 8:
-            document.getElementById("bl4").style.display = "flex";
-            document.getElementById("bl5").style.display = "flex";
-            document.getElementById("bl6").style.display = "flex";
-            document.getElementById("bl7").style.display = "flex";
-            document.getElementById("bl8").style.display = "flex";
-        break;
+    for(var i = 0; i < 8; i++){
+        if(i < n){
+            document.getElementById("bl"+ i).style.display = "flex"
+        } else{
+            document.getElementById("bl"+ i).style.display = "none"
+        }
     }
 }
 
